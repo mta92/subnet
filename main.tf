8f250766cf18a13d1a5207ba567531aff4b9c0e2
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-29 20:16:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,5 +17,5 @@ resource "azurerm_subnet" "subnet_test" {
   name                 = local.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = local.webtier_address_prefixes
+  address_prefixes     = local.address_prefixes
 }
