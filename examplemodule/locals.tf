e4d7ec31df29491c7a538b89195c6429bf135bdf
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-29 20:33:42 -0400

    Update locals.tf

--- examplemodule/locals.tf
+++ examplemodule/locals.tf
@@ -1,8 +1,8 @@
 locals{
-  resource_group_name="MCIT_RG"
-  location="Canada Central"
-  vnet_name="myVNet"
-  address_space=["10.0.0.0/16"]
-  subnet_name="mySubnet"
-  address_prefixes=["10.0.1.0/24"]
+  resource_group_name
+  location
+  vnet_name
+  address_space
+  subnet_name
+  address_prefixes
 }
