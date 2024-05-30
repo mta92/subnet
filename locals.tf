2b248ef9e36900b196410400a1d59b576e01b286
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-29 20:30:08 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,8 @@
 locals{
   resource_group_name="MCIT_RG"
   location="Canada Central"
+  vnet_name="myVNet"
+  address_space=["10.0.0.0/16"]
+  subnet_name="mySubnet"
+  address_prefixes=["10.0.1.0/24"]
 }
